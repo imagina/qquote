1fa1249abb5f435f4f26417e6291474ac55357bb
YeisonTapia <yftapial@ut.edu.co>
2019-12-06 17:55:56 -0500

    + Add package crud

---
+++ _crud/crudPackages.vue
@@ -0,0 +1,63 @@
+<template></template>
+<script>
+  export default {
+    computed: {
+      crudData() {
+        return {
+          apiRoute: 'apiRoutes.qquote.packages',
+          permission: 'iquote.packages',
+          create: {
+            title: this.$tr('qquote.layout.newPackage'),
+          },
+          read: {
+            columns: [
+              {name: 'id', label: this.$tr('ui.form.id'), field: 'id', style: 'width: 50px'},
+              {name: 'name', label: this.$tr('ui.form.name'), field: 'name', align: 'left'},
+              {name: 'actions', label: this.$tr('ui.form.actions'), align: 'right'},
+            ],
+            requestParams: {
+              include:'products'
+            }
+          },
+          update: {
+            title: this.$tr('qquote.layout.updatePackage'),
+            requestParams: {
+              include:'products'
+            }
+          },
+          delete: true,
+          formLeft: {
+            name: {
+              label: `${this.$tr('ui.form.name')}*`,
+              value: '',
+              type: 'text',
+              isTranslatable: true,
+              rules: [
+                val => !!val || this.$tr('ui.message.fieldRequired')
+              ],
+            },
+            description: {
+              label: `${this.$tr('ui.form.description')}*`,
+              value: '',
+              type: 'textarea',
+              isTranslatable: true,
+              rules: [
+                val => !!val || this.$tr('ui.message.fieldRequired')
+              ],
+            },
+            products: {
+              label: this.$trp('qquote.layout.products'),
+              value: [],
+              type: 'multiSelect',
+              loadOptions: {
+                apiRoute: 'apiRoutes.qquote.products',
+                select: {label: 'name', id: 'id'},
+              },
+            },
+          },
+          formRight: {},
+        }
+      }
+    }
+  }
+</script>
