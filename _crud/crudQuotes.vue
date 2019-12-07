d7e186249459ebe9013694edbc00feea0528858b
YeisonTapia <yftapial@ut.edu.co>
2019-12-07 17:56:38 -0500

    + Add quotes crud

---
+++ _crud/crudQuotes.vue
@@ -0,0 +1,33 @@
+<template></template>
+<script>
+  export default {
+    computed: {
+      crudData() {
+        return {
+          apiRoute: 'apiRoutes.qquote.quotes',
+          permission: 'iquote.quotes',
+          create: {
+            title: this.$tr('qquote.layout.newQuote'),
+            to: 'qquote.admin.quotesCreate',
+          },
+          read: {
+            columns: [
+              {name: 'id', label: this.$tr('ui.form.id'), field: 'id', style: 'width: 50px'},
+              {name: 'firstName', label: this.$tr('ui.form.firstName'), field: 'firstName'},
+              {name: 'lastName', label: this.$tr('ui.form.lastName'), field: 'lastName'},
+              {name: 'email', label: this.$tr('ui.form.email'), field: 'email'},
+              {name: 'phone', label: this.$tr('ui.form.phone'), field: 'phone'},
+              {name: 'actions', label: this.$tr('ui.form.actions'), align: 'left', style: 'width: 50px'},
+            ],
+            requestParams: {}
+          },
+          update: {
+            title: this.$tr('qquote.layout.updateQuote'),
+            to : 'qquote.admin.quotesShow'
+          },
+          delete: true,
+        }
+      }
+    }
+  }
+</script>
