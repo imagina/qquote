f127b96245e47147eb01bb89c85e340cfb9d9006
YeisonTapia <yftapial@ut.edu.co>
2019-12-10 18:01:55 -0500

    + Add index page for products

---
+++ _pages/admin/products/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="pageId">
+    <!---Component CRUD-->
+    <crud :crud-data="import('@imagina/qquote/_crud/crudProducts')" />
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
