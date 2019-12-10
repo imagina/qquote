6414aba11ca5c1ea333042a7f47e0ec06fc9d971
YeisonTapia <yftapial@ut.edu.co>
2019-12-10 18:02:54 -0500

    + Add create page for products

---
+++ _pages/admin/products/create.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="q-layout-page layout-padding">
+    <div class="q-mb-lg backend-page">
+      <div class="row q-gutter-y-md">
+        <div class="col-12">
+          <div class="box">
+            <productForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import productForm from '@imagina/qquote/_components/admin/products/form'
+  
+  export default {
+    components:{
+      productForm
+    }
+  }
+</script>
