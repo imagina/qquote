156545fed416cab6700d43c3cb46e0eea0fdb954
YeisonTapia <yftapial@ut.edu.co>
2019-12-10 18:03:31 -0500

    + Add edit page for characteristics

---
+++ _pages/admin/characteristics/edit.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="q-layout-page layout-padding">
+    <div class="q-mb-lg backend-page">
+      <div class="row q-gutter-y-md">
+        <div class="col-12">
+          <div class="box">
+            <formCharacteristics
+              :characteristicId="$route.params.characteristicId"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import formCharacteristics from '@imagina/qquote/_components/admin/characteristics/form'
+
+  export default {
+    components:{
+      formCharacteristics
+    },
+  }
+</script>
