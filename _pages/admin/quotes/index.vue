4f205243f2d19a72bb5a6d0029de74410d1cf001
YeisonTapia <yftapial@ut.edu.co>
2019-12-12 18:05:13 -0500

    + Add index page for quotes

---
+++ _pages/admin/quotes/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="pageId">
+    <!---Component CRUD-->
+    <crud :crud-data="import('@imagina/qquote/_crud/crudQuotes')" />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "index"
+  }
+</script>
+
+<style scoped>
+
+</style>
