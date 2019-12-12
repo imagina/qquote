12efeaeae7093a532c25c58dc361d74592948cd6
YeisonTapia <yftapial@ut.edu.co>
2019-12-12 18:05:32 -0500

    + Add create page for quotes

---
+++ _pages/admin/quotes/create.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <quotaStepper />
+  </div>
+</template>
+
+<script>
+  import quotaStepper from '@imagina/qquote/_components/frontend/quotes/quotaStepper'
+  export default {
+    components:{
+      quotaStepper
+    },
+    created(){
+      this.$store.dispatch('qquoteQuotation/reset_state')
+    },
+  }
+</script>
