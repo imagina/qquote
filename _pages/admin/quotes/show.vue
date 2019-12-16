e00c06dc1fc785fd453c99a2c389e1253cddd51a
YeisonTapia <yftapial@ut.edu.co>
2019-12-16 18:06:43 -0500

    + Add show page for quotes

---
+++ _pages/admin/quotes/show.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <quotaStepper :quotaId="$route.params.id"/>
+  </div>
+</template>
+
+<script>
+  import quotaStepper from '@imagina/qquote/_components/frontend/quotes/quotaStepper'
+  export default {
+    components:{
+      quotaStepper
+    },
+    created(){
+      this.$store.dispatch('qquoteQuotation/reset_state')
+    },
+  }
+</script>
