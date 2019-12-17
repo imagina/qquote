baa402dce6510d349c091dda180db082d2cc3606
YeisonTapia <yftapial@ut.edu.co>
2019-12-17 18:17:54 -0500

    + Add component field component

---
+++ _components/frontend/quotes/fieldContainer.vue
@@ -0,0 +1,64 @@
+<template>
+  <q-list bordered>
+    <q-expansion-item
+      dense-toggle
+      header-class="text-primary bg-grey-3 "
+      expand-icon-class="text-primary">
+      <template v-slot:header>
+        <q-checkbox v-model="characteristic.checked" />
+        <q-item-section>
+          <b>{{characteristic.name}}</b>
+        </q-item-section>
+      </template>
+      <q-card>
+        <q-card-section>
+          <selectionQuote
+            :characteristic="characteristic"
+            v-if="characteristic.type == 1"/>
+          <checkboxQuote
+            :characteristic="characteristic"
+            v-if="characteristic.type == 2"/>
+          <valueQuote
+            :characteristic="characteristic"
+            v-if="characteristic.type == 3"/>
+          <numberQuote
+            :characteristic="characteristic"
+            v-if="characteristic.type == 4"/>
+        </q-card-section>
+      </q-card>
+    </q-expansion-item>
+  </q-list>
+</template>
+
+<script>
+  export default {
+    name: 'fieldContainer',
+    props:{
+      characteristic:{
+        type: Object,
+        default: () => {}
+      }
+    },
+    components:{
+      selectionQuote: () => import('@imagina/qquote/_components/frontend/quotes/fields/iSelect'),
+      checkboxQuote: () => import('@imagina/qquote/_components/frontend/quotes/fields/iCheckbox'),
+      valueQuote: () => import('@imagina/qquote/_components/frontend/quotes/fields/iValue'),
+      numberQuote: () => import('@imagina/qquote/_components/frontend/quotes/fields/iNumber'),
+      optionsQuote: () => import('@imagina/qquote/_components/frontend/quotes/fields/iOption')
+    },
+  }
+</script>
+
+<style>
+  .bg-primary-lighten{
+    background-color: var(--q-color-primary);
+    
+    background-image: linear-gradient(
+      0deg,
+      rgba(255,255,255,0.88) 0%,
+      rgba(255,255,255,0.88) 100%);
+  }
+  
+  
+
+</style>
