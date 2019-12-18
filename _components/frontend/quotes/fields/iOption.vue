6cc59628e805a6f3eefca2c253847cc7ce23b14c
YeisonTapia <yftapial@ut.edu.co>
2019-12-18 18:19:07 -0500

    + Add component option field

---
+++ _components/frontend/quotes/fields/iOption.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    option
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ioption",
+    props:{
+      characteristic:{
+        type: Object,
+        default: () => {}
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
