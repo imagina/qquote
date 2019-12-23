e3d4e2bbb0aa5bfb8f913030dd2c41e5e1c99270
YeisonTapia <yftapial@ut.edu.co>
2019-12-23 11:57:10 -0500

    + add front page, for create quota in page front

--- _config/frontendPages.js
+++ _config/frontendPages.js
@@ -1,3 +1,13 @@
 export default {
-
+  quotesCreate: {
+    permission: null,
+    activated: true,
+    path: '/',
+    name: 'qquote.frontend.quotesCreate',
+    page: () => import('@imagina/qquote/_pages/frontend/quotes/create'),
+    layout: () => import('src/layouts/master.vue'),
+    title: 'qquote.sidebar.adminQuotes',
+    icon: 'fas fa-calculator',
+    authenticated: null,
+  },
 }
