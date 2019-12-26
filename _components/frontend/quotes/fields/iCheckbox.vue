4eca8dea6435ed8dba6582594f219e73dca48acf
YeisonTapia <yftapial@ut.edu.co>
2019-12-26 12:00:08 -0500

    + Update icheckbox component

--- _components/frontend/quotes/fields/iCheckbox.vue
+++ _components/frontend/quotes/fields/iCheckbox.vue
@@ -1,22 +1,20 @@
 <template>
   <div class="row">
-    <div class="col-md-8 q-pr-md flex content-end">
+    <div class="col-xs-12 col-md-8 q-pr-md flex content-end">
       <q-checkbox
         dense
         v-model="characteristic.model"
         :label="`${characteristic.name} (${this.$trc(characteristic.price)})`" />
     </div>
-    <div class="col-md-4">
+    <div class="col-xs-12 col-md-4">
       {{$tr('qquote.layout.labels.total')}}
-      <q-input
-        dense
-        filled
-        outlined
-        v-model="characteristic.model * characteristic.price"
-        disable
-        prefix="$" />
+
+      <div style="border-radius: 5px; border: 1px solid silver; padding: 9px; font-size: 24px" class="text-primary text-bold">
+        {{$trc( (characteristic.model * characteristic.price) || 0 )}}
+      </div>
+
     </div>
-    <div class="col-md-12 q-mt-md">
+    <div class="col-xs-12 col-md-12 q-mt-md">
       <div v-if="characteristic.children">
         <div v-for="(item, index) in characteristic.children">
           <div v-if="characteristic.model">
