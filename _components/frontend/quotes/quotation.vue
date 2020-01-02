9ebd6fcbe4131b10f6e78472a7c17bc9a55064cd
YeisonTapia <yftapial@ut.edu.co>
2020-01-02 12:03:18 -0500

    + Update quotation component

--- _components/frontend/quotes/quotation.vue
+++ _components/frontend/quotes/quotation.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="relative-position">
+    
+    <div class="row">
+      <div class="col-xs-12 bg-grey-2 text-primary q-px-sm q-py-lg q-mb-md rounded-borders">
+        <div class="flex justify-start">
+          <div>
+            <q-icon name="arrow_right_alt" size="xl"/>
+          </div>
+          <div class="q-pt-xs">
+            <div class="text-h5 text-bold">{{$store.state.qquoteQuotation.package.name}}</div>
+            <div class="text-bold">Paquete Seleccionado</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
     <div class="row">
       <div class="col-12 q-ma-none q-pa-none">
         <q-list class="rounded-borders">
           <div
             v-for="(product, index) in products"
             :key="index">
-            <q-list bordered>
+            <q-list >
             <q-expansion-item
-              header-class="bg-primary text-white"
+              header-class="bg-primary text-white rounded-borders"
               expand-icon-class="text-white">
               <template v-slot:header>
                 <q-item-section avatar class="q-py-md q-px-md" v-if="false">
