b0aa8b958547063f46e102d7ec5fc28ec215b0dd
YeisonTapia <yftapial@ut.edu.co>
2020-02-04 17:15:36 -0500

    + Add component calcTotal for generate computed property using calc function.

---
+++ _components/frontend/quotes/calcTotal.vue
@@ -0,0 +1,23 @@
+<template>
+  <span>
+    {{$trc(getTotal|| 0 , $store.state.qquoteQuotation.options.currency.label)}}
+  </span>
+</template>
+
+<script>
+  import { calTotal } from '@imagina/qquote/_utils/index'
+  export default {
+    props:{
+      product:{
+        type: Object,
+        required: true,
+        default: () => {}
+      }
+    },
+    computed:{
+      getTotal(){
+        return calTotal([this.product])
+      }
+    }
+  }
+</script>
